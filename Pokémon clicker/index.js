6fd9a99b6725da1b5236ea48f07fe2870a26700d
Madguy147 <gabriel.gerim.marozi@gmail.com>
2025-06-11 18:33:04 -0300

    Delete Pokémon clicker directory

--- Pokémon clicker/index.js
+++
@@ -1,9 +0,0 @@
-const elementoClicavel = document.querySelector('.elemento-clicavel');
-const valor = document.getElementById('valor');
-
-let contador = 0;
-
-elementoClicavel.addEventListener('click', () => {
-    contador++;
-    valor.textContent = contador;
-});
